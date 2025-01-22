cf416f5aa5c3a9e780e4e969754335c8f8653ed6
Ryan Johnson <rymj07@outlook.com>
2025-01-22 08:29:03 -0700

    Added Files

---
+++ robot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import wpilib as wp
+
+class MyRobot(wp.TimedRobot):
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        # defines the camera function
+        wp.CameraServer.launch()
+
+        self.controller = wp.XboxController(0)
+        self.timer = wp.Timer()
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.timer.restart()
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+
+    def teleopInit(self):
+        """This function is called once each time the robot enters teleoperated mode."""
+
+    def teleopPeriodic(self):
+        """This function is called periodically during teleoperated mode."""
+        
+
+    def testInit(self):
+        """This function is called once each time the robot enters test mode."""
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+
+
+if __name__ == "__main__":
+    wp.run(MyRobot)
